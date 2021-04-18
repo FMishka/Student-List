2389e478b54d704fc677b44dc537a85175d29f53
FMishka <51850577+FMishka@users.noreply.github.com>
2021-04-18 20:01:17 +0300

    Add files via upload

---
+++ StudentList v2.0/StudentList/Initializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace StudentList
+{
+    static class Initializer
+    {
+        const string fileName = @"\std.txt";
+        public static void Save(string path, Student[] students)
+        {
+            using (StreamWriter stream = new StreamWriter(path + fileName))
+            {
+                for (int i = 0; i < StudentHandler.Count(students); i++)
+                {
+                    stream.WriteLine(students[i].id.ToString() + " " + students[i].LastName + " " + students[i].FristName + " " + students[i].comment);
+                }
+            }
+        }
+        public static Student[] Load(string path)
+        {
+            Student[] students = new Student[0];
+            try
+            {
+                using (StreamReader stream = new StreamReader(path + fileName))
+                {
+                    string line = "";
+                    string[] loadingStudent = new string[4];
+                    while ((line = stream.ReadLine()) != null)
+                    {
+                        loadingStudent = line.Split(" ");
+                        Student student = new Student { id = Convert.ToUInt32(loadingStudent[0]), LastName = loadingStudent[1], FristName = loadingStudent[2], comment = loadingStudent[3] };
+                        StudentHandler.AddStudent(ref students, student);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Directory.CreateDirectory(path);
+            }
+            return students;
+        }
+    }
+}
---
+++ StudentList v2.0/StudentList/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentList
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        public uint id;
+        public string comment;
+        public string FristName
+        {
+            set
+            {
+                if (value == null)
+                {
+                    firstName = "";
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                if (value == null)
+                {
+                    lastName = "";
+                }
+                else
+                {
+                    lastName = value;
+                }
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+
+    }
+}
